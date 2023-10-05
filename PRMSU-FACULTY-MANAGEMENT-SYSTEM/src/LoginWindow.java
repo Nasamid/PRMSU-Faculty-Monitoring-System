7ffe97bee97c3b735dcca180e7037fba4e3a61d7
Ralph <justinefar00@gmail.com>
2023-10-05 10:05:12 +0700

    Logib window redesign

--- PRMSU-FACULTY-MANAGEMENT-SYSTEM/src/LoginWindow.java
+++ PRMSU-FACULTY-MANAGEMENT-SYSTEM/src/LoginWindow.java
@@ -1,117 +1,225 @@
-import java.awt.BorderLayout;
+
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.Border;
+import javax.swing.BorderFactory;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
-
-
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
 public class LoginWindow {
-
-    //NEED SOMEONE NA PAGANDAHIN PA
-    //Add Icons, images, basta estetik
-
-    //Instantiation of GUI components
+    
     JFrame LoginWindow;
-    JPanel PTextHolder, PButtonHolder;
-
-    JTextField StaffID ;
-    JPasswordField StaffPass;
-
-    JLabel COEText, ProgNameText;
-
-    JButton LoginButton;
-
-    LoginWindow(){
-
-        LoginWindow = new JFrame("Faculty Monitoring System");
-        LoginWindow.setSize(1380, 800);
-        LoginWindow.setLayout(new BorderLayout());
-
-        PTextHolder = new JPanel();
-        PButtonHolder = new JPanel();
-        //PTextHolder.setBackground(Color.red);
-        //PButtonHolder.setBackground(Color.blue);
-        PButtonHolder.setLayout(new GridBagLayout()); // Use GridBagLayout
-        PTextHolder.setLayout(new BorderLayout());
-
-        //PTextHolder.setPreferredSize(new Dimension(1360, 400));
-       // PButtonHolder.setPreferredSize(new Dimension(1360, 400));
-
-        StaffID = new JTextField();
-        StaffPass = new JPasswordField();
-        LoginButton = new JButton();
-
-        Border line = new LineBorder(Color.BLACK);
-        Border margin = new EmptyBorder(5, 15, 5, 15);
-        Border compound = new CompoundBorder(line, margin);
-
-        COEText = new JLabel("College of Engineering", null, SwingConstants.CENTER);
-        ProgNameText = new JLabel("Faculty Monitoring System", null, SwingConstants.CENTER);
-
-        COEText.setPreferredSize(new Dimension(1360, 40));
-        COEText.setFont(new Font("Lucida Bright", Font.BOLD, 35));
-
-        ProgNameText.setPreferredSize(new Dimension(1360, 40));
-        ProgNameText.setFont(new Font("Lucida Bright", Font.BOLD, 35));
-
-        StaffID.setPreferredSize(new Dimension(300, 40));
-        StaffID.setFont(new Font("Lucida Bright", Font.BOLD, 28));
-
-        StaffPass.setPreferredSize(new Dimension(300, 40));
-        StaffPass.setFont(new Font("Lucida Bright", Font.BOLD, 28));
-
-        LoginButton.setPreferredSize(new Dimension(200, 40));
-        LoginButton.setText("Login");
-        LoginButton.setFont(new Font("Lucida Bright", Font.BOLD, 12));
-        LoginButton.setFocusable(false);
-        LoginButton.setForeground(Color.BLACK);
-        LoginButton.setBackground(Color.decode("#00b4d8"));
-        LoginButton.setBorder(compound);
-
-        PTextHolder.add(COEText, BorderLayout.CENTER);
-        PTextHolder.add(ProgNameText, BorderLayout.SOUTH);
-
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 5, 5, 5);
-
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        PButtonHolder.add(StaffID, gbc);
-
-        gbc.gridy = 1;
-        PButtonHolder.add(StaffPass, gbc);
-
-        gbc.gridy = 2;
-        PButtonHolder.add(LoginButton, gbc);
-
-        LoginWindow.add(PTextHolder, BorderLayout.NORTH);
-        LoginWindow.add(PButtonHolder, BorderLayout.CENTER);
+    JPanel PTextHolder, PButtonHolder,ImgHolder;
+    JPanel PTextM,PTextV,PTextQP;
 
 
-        //Action Listener lang para sa button. Pag tama credentials magcloclose tong
-        //Login Page tas bubukas Homepage
-        //Need someone magimplement ng JLabel na nagsasabi pag wrong password o wrong ID
-        LoginButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                String ID = StaffID.getText().toString();
-                String Pass = StaffPass.getText().toString(); //Someone please implement .getpassword instead
-                
-                if (ID.equals("admin") && Pass.equals("password")) {
+    //JPasswordField StaffPass;
+
+    JLabel UnivText,TitleMission,TitleVision,TitleQP,TitleQP2;
+    JLabel mtext1,mtext2,mtext3,mtext4,mtext5,mtext6,mtext7,mtext8,mtext9;
+    JLabel vtext1, vtext2,vtext3,vtext4;
+    JLabel qptext1,qptext2,qptext3,qptext4,qptext5,qptext6,qptext7,qptext8,qptext9;
+    
+   JButton LoginButton;
+   JTextField Login ;
+    
+   LoginWindow(){  
+    LoginWindow = new JFrame("Faculty Monitoring System");
+    
+    PTextHolder = new JPanel();
+    PButtonHolder = new JPanel();
+    PTextM = new JPanel();
+    PTextV = new JPanel();
+    PTextQP = new JPanel();
+    
+    
+    
+    
+    
+    
+            
+    //Border line = new LineBorder(Color.BLACK);
+    //Border margin = new EmptyBorder(5, 15, 5, 15);
+    //Border compound = new CompoundBorder(line, margin);
+    
+    UnivText = new JLabel("President Ramon Magsaysay State University",null,SwingConstants.LEADING);
+    UnivText.setBounds(10, 15, 1000,40);
+    UnivText.setFont(new Font("Arial", Font.BOLD, 35));
+    
+    Login = new JTextField("");
+    Login.setBounds(1010,25,150,30);
+    
+    LoginButton = new JButton("Login");
+    LoginButton.setBackground(Color.GRAY);
+    LoginButton.setForeground(Color.WHITE);
+    LoginButton.setBounds(1160,25,100,30);
+    
+    TitleMission = new JLabel("MISSION",null,SwingConstants.CENTER);
+    TitleMission.setBounds(25, 25, 250,40);
+    TitleMission.setFont(new Font("Arial", Font.BOLD, 35));
+    
+    mtext1 = new JLabel("The PRMSU shall primarily",null,SwingConstants.CENTER);
+    mtext1.setBounds(25, 100, 250,40);
+    mtext1.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    mtext2 = new JLabel("provide advance and higher",null,SwingConstants.CENTER);
+    mtext2.setBounds(25, 140, 250,40);
+    mtext2.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    mtext3 = new JLabel("professional, technical, and",null,SwingConstants.CENTER);
+    mtext3.setBounds(25, 180, 250,40);
+    mtext3.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    mtext4 = new JLabel("special instructions in various",null,SwingConstants.CENTER);
+    mtext4.setBounds(25, 220, 250,40);
+    mtext4.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    mtext5 = new JLabel("disciplines; undertake research,",null,SwingConstants.CENTER);
+    mtext5.setBounds(25, 260, 250,40);
+    mtext5.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    mtext6 = new JLabel("extension and income generation",null,SwingConstants.CENTER);
+    mtext6.setBounds(25, 300, 250,40);
+    mtext6.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    mtext7 = new JLabel("programs for the sustainable",null,SwingConstants.CENTER);
+    mtext7.setBounds(25, 340, 250,40);
+    mtext7.setFont(new Font("Arial", Font.BOLD, 15));
+
+    mtext8 = new JLabel("development of Zambales,",null,SwingConstants.CENTER);
+    mtext8.setBounds(25, 380, 250,40);
+    mtext8.setFont(new Font("Arial", Font.BOLD, 15));
+
+    mtext9 = new JLabel(" the region and the country. ",null,SwingConstants.CENTER);
+    mtext9.setBounds(25, 420, 250,40);
+    mtext9.setFont(new Font("Arial", Font.BOLD, 15));    
+    
+    TitleVision = new JLabel("VISION",null,SwingConstants.CENTER);
+    TitleVision.setBounds(25, 25, 250,40);
+    TitleVision.setFont(new Font("Arial", Font.BOLD, 35));
+    
+    
+    vtext1 = new JLabel("PRMSU shall be a premier",null,SwingConstants.CENTER);
+    vtext1.setBounds(25, 100, 250,40);
+    vtext1.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    vtext2 = new JLabel("learner-centered and proactive",null,SwingConstants.CENTER);
+    vtext2.setBounds(25, 140, 250,40);
+    vtext2.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    vtext3 = new JLabel("university in a digital",null,SwingConstants.CENTER);
+    vtext3.setBounds(25, 180, 250,40);
+    vtext3.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    vtext4 = new JLabel(" and global society. ",null,SwingConstants.CENTER);
+    vtext4.setBounds(25, 220, 250,40);
+    vtext4.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    
+    TitleQP = new JLabel("QUALITY",null,SwingConstants.CENTER);
+    TitleQP.setBounds(25, 25, 250,40);
+    TitleQP.setFont(new Font("Arial", Font.BOLD, 35));
+    
+    TitleQP2 = new JLabel("POLICY",null,SwingConstants.CENTER);
+    TitleQP2.setBounds(25, 60, 250,40);
+    TitleQP2.setFont(new Font("Arial", Font.BOLD, 35));
+
+    qptext1 = new JLabel("The President Ramon Magsaysay",null,SwingConstants.CENTER);
+    qptext1.setBounds(25, 100, 250,40);
+    qptext1.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    qptext2 = new JLabel("State University is committed to ",null,SwingConstants.CENTER);
+    qptext2.setBounds(25, 140, 250,40);
+    qptext2.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    qptext3 = new JLabel("continually strive for excellence",null,SwingConstants.CENTER);
+    qptext3.setBounds(25, 180, 250,40);
+    qptext3.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    qptext4 = new JLabel("in instruction, research,",null,SwingConstants.CENTER);
+    qptext4.setBounds(25, 220, 250,40);
+    qptext4.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    qptext5 = new JLabel("extension and production to",null,SwingConstants.CENTER);
+    qptext5.setBounds(25, 260, 250,40);
+    qptext5.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    qptext6 = new JLabel("strengthen global competitiveness",null,SwingConstants.CENTER);
+    qptext6.setBounds(25, 300, 250,40);
+    qptext6.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    qptext7 = new JLabel("adhering to quality standards",null,SwingConstants.CENTER);
+    qptext7.setBounds(25, 340, 250,40);
+    qptext7.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    qptext8 = new JLabel("for the utmost satisfaction",null,SwingConstants.CENTER);
+    qptext8.setBounds(25, 380, 250,40);
+    qptext8.setFont(new Font("Arial", Font.BOLD, 15));
+    
+    qptext9 = new JLabel("of its valued customers.",null,SwingConstants.CENTER);
+    qptext9.setBounds(25, 420, 250,40);
+    qptext9.setFont(new Font("Arial", Font.BOLD, 15));
+   
+    
+
+   // LoginButton.setBounds(600, 200, 150, 75);
+    //LoginButton.setText("Login");
+   // LoginButton.setFont(new Font("Lucida Bright", Font.BOLD, 12));
+   // LoginButton.setFocusable(false);
+    //LoginButton.setForeground(Color.BLACK);
+   // LoginButton.setBackground(Color.decode("#00b4d8"));
+    //LoginButton.setBorder(compound);
+
+    //StaffID.setBounds(475, 50, 400, 50);
+    //StaffID.setFont(new Font("Lucida Bright", Font.BOLD, 28));
+
+    //StaffPass.setBounds(475, 125, 400, 50);
+    //StaffPass.setFont(new Font("Lucida Bright", Font.BOLD, 28));
+    PTextHolder.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    PTextHolder.setBackground(Color.yellow);
+    PTextHolder.setBounds(0,0,1365, 75);
+    PTextHolder.setLayout(null);
+    
+    PTextM.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    PTextM.setBounds(150,200,300, 500);
+    PTextM.setLayout(null);
+
+    PTextV.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    PTextV.setBounds(525,200,300, 500);
+    PTextV.setLayout(null);
+
+    PTextQP.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    PTextQP.setBounds(900,200,300, 500);
+    PTextQP.setLayout(null);
+    
+    LoginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) 
+            {
+                String ID = Login.getText();
+                if(ID.equals("admin")){
+                    JOptionPane.showMessageDialog(null, "WELCOME!");
                     System.out.println("Welcome!");
                     HomeWindow homepage = new HomeWindow();
                     LoginWindow.dispose();
@@ -119,18 +227,72 @@ public class LoginWindow {
                 }
                 else {
                     //FailedLogin.setVisible(true);
+                    JOptionPane.showMessageDialog(null, "Invalid Username.");
                     System.out.println(ID);
-                    System.out.println(Pass);
                     System.out.println("Invalid Credentials");
                 }
             }
         });
 
+    //PButtonHolder.setBounds(6, 150, 1360, 500);
+    //PButtonHolder.setLayout(null);
+        
+    PTextHolder.add(UnivText);
+    PTextHolder.add(LoginButton);
+    PTextHolder.add(Login);
+    
+    PTextM.add(TitleMission);
+    PTextM.add(mtext1);
+    PTextM.add(mtext2);
+    PTextM.add(mtext3);
+    PTextM.add(mtext4);
+    PTextM.add(mtext5);
+    PTextM.add(mtext6);
+    PTextM.add(mtext7);
+    PTextM.add(mtext8);
+    PTextM.add(mtext9);
 
-        //Essentials lang ng JFrame
-        LoginWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        LoginWindow.setResizable(true);
-        //LoginWindow.setIconImage(ProgramIcon.getImage());
-        LoginWindow.setVisible(true);
+    PTextV.add(TitleVision);
+    PTextV.add(vtext1);
+    PTextV.add(vtext2);
+    PTextV.add(vtext3);
+    PTextV.add(vtext4);
+    
+    
+    
+    PTextQP.add(TitleQP);
+    PTextQP.add(TitleQP2);
+    PTextQP.add(qptext1);
+    PTextQP.add(qptext2);
+    PTextQP.add(qptext3);
+    PTextQP.add(qptext4);
+    PTextQP.add(qptext5);
+    PTextQP.add(qptext6);
+    PTextQP.add(qptext7);
+    PTextQP.add(qptext8);
+    PTextQP.add(qptext9);
+
+
+   // PButtonHolder.add(StaffPass);
+   // PButtonHolder.add(StaffID);
+   // PButtonHolder.add(LoginButton);
+
+    LoginWindow.add(PTextHolder);
+    LoginWindow.add(PTextM);
+    LoginWindow.add(PTextV);
+    LoginWindow.add(PTextQP);
+    LoginWindow.add(PButtonHolder);
+    
+    //essential
+    LoginWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    LoginWindow.setExtendedState(JFrame.MAXIMIZED_BOTH); //Ito yung line of code na nagfufullscreen ng frame
+    LoginWindow.setResizable(false);
+    LoginWindow.setLayout(null);
+    //LoginWindow.setIconImage(ProgramIcon.getImage());
+    LoginWindow.setVisible(true);
+}
+    public static void main(String[] args) {
+        new LoginWindow();
     }
+    
 }
